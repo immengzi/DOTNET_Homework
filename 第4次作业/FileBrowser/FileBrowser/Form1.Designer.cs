22d3164b4e1cd709edb441e7f44a94a07d623b73
immengzi <immengzi@outlook.com>
2023-10-30 19:40:57 +0800

    add 4th homework

---
+++ 第4次作业/FileBrowser/FileBrowser/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace FileBrowser
+{
+    partial class FileBrowser
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FileBrowser));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(2, 82);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(796, 367);
+            this.splitContainer1.SplitterDistance = 265;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(3, 3);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(259, 361);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(3, 3);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(521, 361);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // FileBrowser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FileBrowser";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ 第4次作业/FileBrowser/FileBrowser/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FileBrowser
+{
+    public partial class FileBrowser : Form
+    {
+        private string rootDirectory;
+        private TreeNode currentDirectoryNode;
+        public FileBrowser()
+        {
+            InitializeComponent();
+            // 因为 C 盘容易出现权限问题，所以以 D 盘为根目录...
+            rootDirectory = "D:"; // 根目录路径
+            PopulateTreeView(rootDirectory, null);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 设置窗口布局
+            splitContainer1.Dock = DockStyle.Fill;
+            // 设置树形视图
+            treeView1.Dock = DockStyle.Fill;
+            treeView1.AfterSelect += treeView1_AfterSelect;
+            // 设置列表视图
+            listView1.Dock = DockStyle.Fill;
+            listView1.View = View.Details;
+            listView1.Columns.Add("名称");
+            listView1.Columns.Add("类型");
+            listView1.DoubleClick += listView1_DoubleClick;
+
+
+            splitContainer1.Panel1.Controls.Add(treeView1);
+            splitContainer1.Panel2.Controls.Add(listView1);
+            Controls.Add(splitContainer1);
+
+        }
+
+        private void PopulateTreeView(string directory, TreeNode parentNode)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(directory);
+            TreeNode directoryNode = new TreeNode(directoryInfo.Name);
+
+            if (parentNode == null)
+            {
+                treeView1.Nodes.Add(directoryNode);
+            }
+            else
+            {
+                parentNode.Nodes.Add(directoryNode);
+            }
+
+            if (currentDirectoryNode == null)
+            {
+                currentDirectoryNode = directoryNode;
+            }
+
+            try
+            {
+                foreach (var dir in directoryInfo.GetDirectories())
+                {
+                    PopulateTreeView(dir.FullName, directoryNode);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // 忽略无法访问的文件夹
+            }
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            treeView1.SelectedNode = e.Node;
+            string selectedPath = GetFullPath(e.Node);
+
+            // 清空列表视图
+            listView1.Items.Clear();
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(selectedPath);
+
+            foreach (var dir in directoryInfo.GetDirectories())
+            {
+                ListViewItem item = new ListViewItem(dir.Name);
+                item.SubItems.Add("文件夹");
+                listView1.Items.Add(item);
+            }
+
+            foreach (var file in directoryInfo.GetFiles())
+            {
+                ListViewItem item = new ListViewItem(file.Name);
+                item.SubItems.Add("文件");
+                listView1.Items.Add(item);
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                string selectedPath = GetFullPath(treeView1.SelectedNode) + "\\" + listView1.SelectedItems[0].Text;
+                if (Path.GetExtension(selectedPath).ToLower() == ".txt")
+                {
+                    try
+                    {
+                        using (StreamReader sr = new StreamReader(selectedPath))
+                        {
+                            MessageBox.Show(sr.ReadToEnd(), "File Content");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error reading the file: " + ex.Message, "Error");
+                    }
+                }
+            }
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                string selectedItem = listView1.SelectedItems[0].Text;
+                string selectedPath = GetFullPath(treeView1.SelectedNode) + "\\" + selectedItem;
+
+                if (Directory.Exists(selectedPath))
+                {
+                    // 清空列表视图
+                    listView1.Items.Clear();
+
+                    // 更新树形视图
+                    TreeNode selectedNode = treeView1.SelectedNode;
+                    selectedNode.Nodes.Clear();
+                    PopulateTreeView(selectedPath, selectedNode);
+
+                    // 显示文件夹内容
+                    DirectoryInfo directoryInfo = new DirectoryInfo(selectedPath);
+                    foreach (var dir in directoryInfo.GetDirectories())
+                    {
+                        ListViewItem item = new ListViewItem(dir.Name);
+                        item.SubItems.Add("文件夹");
+                        listView1.Items.Add(item);
+                    }
+                    foreach (var file in directoryInfo.GetFiles())
+                    {
+                        ListViewItem item = new ListViewItem(file.Name);
+                        item.SubItems.Add("文件");
+                        listView1.Items.Add(item);
+                    }
+                    // 刷新树形视图
+                    UpdateTreeView();
+                }
+            }
+        }
+
+
+        private string GetFullPath(TreeNode node)
+        {
+            if (node == null)
+                return rootDirectory;
+
+            string path = node.Text;
+            TreeNode currentNode = node;
+
+            while (currentNode.Parent != null)
+            {
+                currentNode = currentNode.Parent;
+                path = currentNode.Text + "\\" + path;
+            }
+
+            return path;
+        }
+
+        private void UpdateTreeView()
+        {
+            treeView1.BeginUpdate();
+            treeView1.Nodes.Clear();
+            PopulateTreeView(rootDirectory, null);
+
+            if (currentDirectoryNode != null)
+            {
+                treeView1.SelectedNode = currentDirectoryNode;
+                currentDirectoryNode.Expand();
+            }
+
+            treeView1.EndUpdate();
+        }
+
+    }
+}
